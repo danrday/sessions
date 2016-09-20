0297fac015b9c02bac2a5bc646515e9d6b8b3aba
Dan Day <danday@gmail.com>
2016-09-20 12:52:26 -0500

    working

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
 
 const routes = require('./routes/')
 const { connect } = require('./db/database')
@@ -13,9 +15,26 @@ app.set('port', port)
 
 // pug configuration
 app.set('view engine', 'pug')
-
+app.locals.company = 'app.locals.company'
 app.locals.errors = {} // errors & body added to avoid guard statements
 app.locals.body = {} // i.e. value=(body && body.name) vs. value=body.name
+
+app.use(session({
+  store: new RedisStore(),
+  secret: 'secretsalt'
+}))
+
+app.use((req, res, next) => {
+  app.locals.email = req.session.email
+  next()
+})
+
+// app.use(({ method, url, headers: { 'user-agent': agent } }, res, next) => {
+//   const timeStamp = new Date()
+//   console.log(`[${timeStamp}] "${cyan(`${method} ${url}`)}" "${agent}"`)
+//   next()
+// })
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
